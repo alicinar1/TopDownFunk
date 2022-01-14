e07d1c09bb36019b9940464bf083ed017e8b6c87
alicinar1 <alicinar1@users.noreply.github.com>
2022-01-14 22:39:35 +0300

    PlayerShoot and Damage System started

---
+++ TopDownFunk/Assets/Scripts/Damage/IDamagable.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TopDownFunk.Damage
+{
+    public static class DamagebleHelper
+    {
+        public static Dictionary<int, IDamagable> DamagableList = new Dictionary<int, IDamagable>();
+
+        public  static void InitializeDamageble(this IDamagable damagable)
+        {
+            DamagableList.Add(damagable.InstanceID, damagable);
+        }
+
+        public static void DestroyDamageble(this IDamagable damagable)
+        {
+            DamagableList.Remove(damagable.InstanceID);
+        }
+    }
+
+    public interface IDamagable
+    {
+        int InstanceID { get; }
+        void Damage();
+    }
+}
---
+++ TopDownFunk/Assets/Scripts/PlayerController/Shoot/PlayerShootingController.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TopDownFunk.PlayerController
+{
+    public class PlayerShootingController : MonoBehaviour
+    {
+
+    }
+}
